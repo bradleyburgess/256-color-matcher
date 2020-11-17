2c129a2cd666f6ee4834b19546be3838a4fe262b
azalus88 <bradleyburgess@gmail.com>
2020-11-17 13:11:59 -0500

    Redesigned swatch area

--- src/components/ResultArea.jsx
+++ src/components/ResultArea.jsx
@@ -8,23 +8,34 @@ import padColorId from "../utils/padColorId.js";
 const styles = {
   container: "mt-5",
   text: "text-center",
-  bigColorId: "text-4xl text-center my-5",
-  swatchArea: "flex flex-row flex-wrap justify-center mt-2",
+  bigColorId:
+    "border-b-8 border-gray-300 py-2 text-4xl text-center w-full flex-shrink-0 flex-grow",
+  swatchArea: "flex flex-row flex-wrap justify-center sm:mt-6",
+  swatchContainer: [
+    "flex",
+    "flex-wrap",
+    "flex-row",
+    "justify-center",
+    "rounded-xl",
+    "border",
+    "shadow",
+    "box-border",
+    "border-gray-300",
+    "overflow-hidden",
+  ],
   swatch: [
+    "box-border",
     "duration-200",
     "flex",
+    "flex-shrink-0",
     "flex-col",
     "font-mono",
     "h-40",
     "items-center",
     "justify-center",
-    "mb-3",
-    "mx-2",
-    "rounded-md",
-    "shadow",
     "text-sm",
     "transition-colors",
-    "w-40",
+    "w-1/2",
   ],
 };
 
@@ -34,29 +45,33 @@ const ResultArea = ({ mode, inputColor, matchColor }) => {
   const matchColorText =
     mode === "hex" ? rgbToHex(matchColor.rgb) : rgbStringify(matchColor);
 
-  debugger;
   return (
     <div className={clsx(styles.container)}>
       <p className={clsx(styles.text)}>
         The closest match to <br />
         <code>{inputColorText}</code> is:
       </p>
-      <p className={clsx(styles.bigColorId)}>
-        <code>{padColorId(matchColor.colorId)}</code>
-      </p>
       <div className={clsx(styles.swatchArea)}>
-        <Swatch
-          mode={mode}
-          inputColorText={inputColorText}
-          inputColor={inputColor}
-        />
-        <Swatch
-          mode={mode}
-          inputColorText={matchColorText}
-          inputColor={matchColor.rgb}
-          colorId={matchColor.colorId}
-          colorName={matchColor.name}
-        />
+        <div
+          className={clsx(styles.swatchContainer)}
+          style={{ width: "300px" }}
+        >
+          <p className={clsx(styles.bigColorId)}>
+            <code>{padColorId(matchColor.colorId)}</code>
+          </p>
+          <Swatch
+            mode={mode}
+            inputColorText={inputColorText}
+            inputColor={inputColor}
+          />
+          <Swatch
+            mode={mode}
+            inputColorText={matchColorText}
+            inputColor={matchColor.rgb}
+            colorId={matchColor.colorId}
+            colorName={matchColor.name}
+          />
+        </div>
       </div>
     </div>
   );
