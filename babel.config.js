b5817ead4ee3fbdd1a9cf857e2d7c2f878a24d50
Bradley Burgess <bradleyburgess@gmail.com>
2022-03-30 14:19:56 +0200

    Removed console.log in prod

--- babel.config.js
+++ babel.config.js
@@ -3,5 +3,8 @@ module.exports = {
     test: {
       plugins: ["@babel/plugin-transform-modules-commonjs"],
     },
+    production: {
+      plugins: ["transform-remove-console"],
+    },
   },
 };
